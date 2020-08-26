7d5659360e245675e9e22f9b05e87b552caa807c
Brandon J. Bjelland <brandoconnor@gmail.com>
2020-08-25 23:55:29 -0700

    fix: relax version constraints to enable terraform 0.13.x compatibility (#108)

--- modules/mig_with_percent/versions.tf
+++ modules/mig_with_percent/versions.tf
@@ -15,7 +15,7 @@
  */
 
 terraform {
-  required_version = "~> 0.12.6"
+  required_version = ">=0.12.6, <0.14"
   required_providers {
     google      = ">= 2.7, <4.0"
     google-beta = ">= 2.7, <4.0"

b00e6af814a2951e841ae65e4fb3ab396f967fa4
uros.simovic <uros.simovic@kiwi.com>
2020-09-09 08:47:39 +0200

    change access_config array to 2D

--- modules/umig/main.tf
+++ modules/umig/main.tf
@@ -56,7 +56,7 @@ resource "google_compute_instance_from_template" "compute_instance" {
     network_ip         = length(var.static_ips) == 0 ? "" : element(local.static_ips, count.index)
 
     dynamic "access_config" {
-      for_each = var.access_config
+      for_each = var.access_config[count.index]
       content {
         nat_ip       = access_config.value.nat_ip
         network_tier = access_config.value.network_tier

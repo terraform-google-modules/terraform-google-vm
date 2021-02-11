46699b48719935aabbd8f0556bb6c207b98ae233
Bogdan Alecu <bogdalecu@gmail.com>
2021-02-11 15:03:50 +0200

    chore: add more outputs to umig

--- modules/umig/outputs.tf
+++ modules/umig/outputs.tf
@@ -19,9 +19,19 @@ output "self_links" {
   value       = google_compute_instance_group.instance_group.*.self_link
 }
 
+output "umig_details" {
+  description = "List of all details for unmanaged instance groups"
+  value       = google_compute_instance_group.instance_group.*
+}
+
 output "instances_self_links" {
   description = "List of self-links for compute instances"
   value       = google_compute_instance_from_template.compute_instance.*.self_link
+}
+
+output "instances_details" {
+  description = "List of all details for compute instances"
+  value       = google_compute_instance_from_template.compute_instance.*
 }
 
 output "available_zones" {

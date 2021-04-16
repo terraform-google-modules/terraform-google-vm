c2ae00d22e811e3fbf9a1b56ae18f394b19995b8
Martin Petkov <MartinPetkov@users.noreply.github.com>
2021-04-16 15:49:46 -0400

    Update modules/compute_instance/variables.tf
    Co-authored-by: Bharath KKB <bharathkrishnakb@gmail.com>

--- modules/compute_instance/variables.tf
+++ modules/compute_instance/variables.tf
@@ -66,6 +66,6 @@ variable "region" {
 
 variable "zone" {
   type        = string
-  description = "Zone where the instances should be created. If not specified, will spread instances across available zones in the region."
+  description = "Zone where the instances should be created. If not specified, instances will be spread across available zones in the region."
   default     = null
 }

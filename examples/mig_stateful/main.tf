a97c11b91afb0fd903915342bfacf7c3428559bc
Eric Zhao <zhaoe@google.com>
2023-04-24 14:52:21 +1000

    fix: update based on pr feedback

--- examples/mig_stateful/main.tf
+++ examples/mig_stateful/main.tf
@@ -15,13 +15,9 @@
  */
 
 provider "google" {
-  project = var.project_id
-  region  = var.region
 }
 
 provider "google-beta" {
-  project = var.project_id
-  region  = var.region
 }
 
 resource "random_string" "suffix" {

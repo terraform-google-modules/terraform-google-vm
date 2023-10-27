677b27b912d4ee4fcc59a4510f35c5f917c29fe1
Awais Malik <awmalik@google.com>
2023-10-27 14:54:54 -0700

    fix: upgraded versions.tf to include minor bumps from tpg v5 (#350)

--- modules/compute_instance/versions.tf
+++ modules/compute_instance/versions.tf
@@ -19,7 +19,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.88, < 5.0"
+      version = ">= 3.88, < 6"
     }
   }
   provider_meta "google" {

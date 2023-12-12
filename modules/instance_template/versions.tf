f1cfcb1dec86c739d865f785ee1c166b73bc7f88
Tom Downes <tpdownes@users.noreply.github.com>
2023-12-12 08:37:57 -0600

    Update modules/instance_template/versions.tf
    Co-authored-by: Andrew Peabody <andrewpeabody@google.com>

--- modules/instance_template/versions.tf
+++ modules/instance_template/versions.tf
@@ -19,7 +19,7 @@ terraform {
   required_providers {
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 5.5"
+      version = ">= 5.5, < 6"
     }
   }
   provider_meta "google" {

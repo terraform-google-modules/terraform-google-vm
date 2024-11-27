ffcfb17d11bcacbd7c2373f43cee2d816fe4d57c
Abhishek Tiwari <abhiwa@google.com>
2024-11-27 10:36:00 +0000

    chore: Update variable's order in variables.tf and update display metadata

--- modules/mig/variables.tf
+++ modules/mig/variables.tf
@@ -22,10 +22,9 @@ variable "project_id" {
   default     = null
 }
 
-variable "hostname" {
-  description = "Hostname prefix for instances"
+variable "region" {
+  description = "The Google Cloud region where the managed instance group resides."
   type        = string
-  default     = "default"
 }
 
 variable "mig_name" {
@@ -34,20 +33,21 @@ variable "mig_name" {
   default     = ""
 }
 
-variable "region" {
-  description = "The Google Cloud region where the managed instance group resides."
+variable "hostname" {
+  description = "Hostname prefix for instances"
   type        = string
-}
-
-variable "instance_template" {
-  description = "Instance template self_link used to create compute instances"
-  type        = string
+  default     = "default"
 }
 
 variable "target_size" {
   description = "The target number of running instances for this managed instance group. This value should always be explicitly set unless this resource is attached to an autoscaler, in which case it should never be set."
   type        = number
   default     = 1
+}
+
+variable "instance_template" {
+  description = "Instance template self_link used to create compute instances"
+  type        = string
 }
 
 variable "target_pools" {

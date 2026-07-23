cb56ae9952f61c9a8a159295faf630ac87713e4f
Yogita Upadhyay <yogitaupadhyay@google.com>
2026-07-23 15:05:00 +0000

    update the image to latest this is required for moss

--- modules/instance_template/main.tf
+++ modules/instance_template/main.tf
@@ -19,7 +19,7 @@
 #########
 
 locals {
-  source_image         = var.source_image != "" ? var.source_image : "rocky-linux-9-optimized-gcp-v20240111"
+  source_image         = var.source_image != "" ? var.source_image : "rocky-linux-9-optimized-gcp-v20260720"
   source_image_family  = var.source_image_family != "" ? var.source_image_family : "rocky-linux-9-optimized-gcp"
   source_image_project = var.source_image_project != "" ? var.source_image_project : "rocky-linux-cloud"
 
